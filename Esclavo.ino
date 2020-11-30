ae4b4156bfd504f7f0fb0c02a5491161bb47839c
albanlisseth <74896225+albanlisseth@users.noreply.github.com>
2020-11-30 04:37:40 -0500

    Add files via upload

---
+++ Esclavo.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+void setup()
+{
+  Wire.begin(4);     // id de esclavo           
+  Wire.onReceive(receiveEvent);  //evento de recepción de mensajes
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);   // comunicación serial        
+}
+
+void loop()
+{
+delay(100);
+}
+void receiveEvent(int bytes){ // evento i2c
+  while(Wire.available())  // si existe mensaje por leer
+  {
+    Serial.println("mensaje recibido");
+    int c = Wire.read();  // recibe dato
+    Serial.print(c);    //envia por comunicación serial 
+  }
+}
+// function that executes whenever data is requested from master
+void requestEvent() {
+ Wire.write("Hi");  /*send string on request */
+}
---
+++ Master.ino
@@ -0,0 +1,35 @@
+#define DEBUG(a) Serial.println(a);
+#include <Wire.h>
+
+String data = "";
+int cont = 0; //variable de conteo
+
+void setup()
+{
+  Wire.begin(); //inicio de comunicación i2c
+  Serial.begin(9600);
+  Serial.println("Master");
+}
+
+void loop()
+{
+  while (Serial.available())
+  {
+    char character = Serial.read();
+    if (character != 13 )
+    {
+      data.concat(character);
+      
+    }
+    else
+    {
+      
+      Wire.beginTransmission(4); // configuración de esclavo a enviar
+      Wire.write("data");  // envio de mensaje
+      Wire.endTransmission(); // fin de comunicación
+      Serial.println("Mensaje Enviado");
+      delay(1000); // espera
+      data = "";
+    }
+  }
+}
